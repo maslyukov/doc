8c520e446e15271bdaa336c8f251fec53a5052d0
Michael Maslyukov <m.maslyukov@gmail.com>
2013-10-31 12:09:21 +0200

    add mind map for several projects

---
+++ MyProjects.mm
@@ -0,0 +1,84 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1383211701105" ID="ID_291265189" MODIFIED="1383212141821" TEXT="Projects">
+<node CREATED="1383212142294" ID="ID_460660023" MODIFIED="1383212156752" POSITION="right" TEXT="Remote sound transfer">
+<node CREATED="1383212889525" ID="ID_771210433" MODIFIED="1383212892465" TEXT="Stram">
+<node CREATED="1383213006732" ID="ID_842852193" MODIFIED="1383213013185" TEXT="Compressing">
+<node CREATED="1383213014037" ID="ID_886939747" MODIFIED="1383213021570" TEXT="mp3, other"/>
+</node>
+</node>
+<node CREATED="1383212893661" ID="ID_1108409208" MODIFIED="1383212904210" TEXT="Platform">
+<node CREATED="1383212908990" ID="ID_1795102182" MODIFIED="1383212917858" TEXT="Windows">
+<node CREATED="1383212923311" ID="ID_1412380085" MODIFIED="1383212937308" TEXT="Tray application"/>
+<node CREATED="1383212937792" ID="ID_167316925" MODIFIED="1383212941724" TEXT="WASAPI"/>
+</node>
+<node CREATED="1383212918287" ID="ID_303642407" MODIFIED="1383212921651" TEXT="Android">
+<node CREATED="1383212943560" ID="ID_721835393" MODIFIED="1383212971766" TEXT="GUI control, native business logic "/>
+<node CREATED="1383212974194" ID="ID_1502354993" MODIFIED="1383212978142" TEXT="OpenSL"/>
+</node>
+</node>
+<node CREATED="1383212906870" ID="ID_71149979" MODIFIED="1383212908234" TEXT="Goal">
+<node CREATED="1383212982195" ID="ID_784801189" MODIFIED="1383212991424" TEXT="Wireless speakers"/>
+<node CREATED="1383212992083" ID="ID_937480411" MODIFIED="1383213003818" TEXT="Remote notifications"/>
+</node>
+</node>
+<node CREATED="1383212150173" ID="ID_347224588" MODIFIED="1383212713808" POSITION="left">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Scene reconstruction
+    </p>
+    <p>
+      from video or photo
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1383212233131" ID="ID_7831632" MODIFIED="1383212242533" TEXT="Technology">
+<node CREATED="1383212591281" ID="ID_1524458017" MODIFIED="1383212594997" TEXT="OpenCV"/>
+<node CREATED="1383212596401" ID="ID_921582029" MODIFIED="1383212598214" TEXT="SLAM"/>
+<node CREATED="1383212598873" ID="ID_151179592" MODIFIED="1383212600293" TEXT="PTAM"/>
+</node>
+<node CREATED="1383212603490" ID="ID_1271962350" MODIFIED="1383212609926" TEXT="Goal">
+<node CREATED="1383212611106" ID="ID_598851735" MODIFIED="1383212704031">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Internet market with 3D object.
+    </p>
+    <p>
+      User can rotate and zoom it
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1383212621235" ID="ID_1749577429" MODIFIED="1383212692755">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Car crash reporting. User can take
+    </p>
+    <p>
+      several&#160;photos of indecent for later
+    </p>
+    <p>
+      analysis it in 3D scene
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</map>
